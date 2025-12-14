3745e188af2fd20fecaaa774666617f8a31b7dc6
Аюбов Анзор <48889137+anzorayubov@users.noreply.github.com>
2025-12-15 00:01:52 +0300

    Update Angular from v17 to v18
    - Update @angular/cli: 17.3.17 -> 18.2.21
    - Update @angular/core: 17.3.12 -> 18.2.14
    - Migrate HTTP modules to provider functions
    - Auto-update data.service.ts and app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
-import { HttpClientModule} from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { HeaderComponent } from './components/header/header.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    CalendarComponent,
-    HeaderComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        CalendarComponent,
+        HeaderComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import {UserNameStorageService} from "./user-name-storage.service";
 import {Observable} from "rxjs";
 import {CodewarsResponse, UserInfo} from "../interfaces";
